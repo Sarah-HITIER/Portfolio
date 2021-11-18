5c6fd6070f589570aa121ad078dd2de18c07d904
Sarah HITIER <sarah.hitier@sfr.fr>
2021-11-18 01:25:30 +0100

    Responsive

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -1,11 +1,28 @@
 (function () {
-    let loader = document.getElementById("logo-loader");
-    let text = document.getElementsByClassName("hero-column");
-    window.setTimeout(function () {
-        loader.classList.remove("logo-loader");
-        loader.classList.add("logo-loader-hero");
-        for(var i=0 ; i<text.length ; i++) {
-            text[i].style.display = "flex";
-        }
-    }, 2000);
+    if (
+        location.pathname === "/public/" ||
+        location.pathname === "/public/index.html"
+    ) {
+        let header = document.getElementById("header");
+        let header_mobile = document.getElementById("header-mobile");
+        let loader = document.getElementById("logo-loader");
+        let text = document.getElementsByClassName("hero-column");
+        let footer = document.getElementById("footer");
+
+        header.style.display = "none";
+        header_mobile.style.display = "none";
+
+        if (footer) footer.style.display = "none";
+
+        window.setTimeout(function () {
+            header.style.display = "flex";
+            header_mobile.style.display = "flex";
+            loader.classList.remove("logo-loader");
+            loader.classList.add("logo-loader-hero");
+            footer.style.display = "flex";
+            for (var i = 0; i < text.length; i++) {
+                text[i].style.display = "flex";
+            }
+        }, 2000);
+    }
 })();
