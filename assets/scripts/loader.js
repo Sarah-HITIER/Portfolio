6d5fe4d0bd47fdc64c8bd95ff014860fb386fb10
Sarah HITIER <sarah.hitier@sfr.fr>
2021-11-19 07:58:35 +0100

    Footer

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -1,3 +1,7 @@
+/**
+ * Retirer le header et le footer pendant le loader puis
+ * d'afficher la page index quand le loader est fini
+ */
 (function () {
     if (
         location.pathname === "/public/" ||
@@ -11,7 +15,6 @@
 
         header.style.display = "none";
         header_mobile.style.display = "none";
-
         if (footer) footer.style.display = "none";
 
         window.setTimeout(function () {
@@ -24,5 +27,26 @@
                 text[i].style.display = "flex";
             }
         }, 2000);
+    }
+})();
+
+/**
+ * Ajouter la classe "active" sur le lien de la page
+ * courante
+ */
+(function () {
+    let real_link = document.getElementById("real_link");
+    let comp_link = document.getElementById("comp_link");
+    let exp_link = document.getElementById("exp_link");
+    if (
+        location.pathname === "/public/benchkatalog.html" ||
+        location.pathname === "/public/etais-la-sauvin.html" ||
+        location.pathname === "/public/quizzy.html"
+    ) {
+        real_link.classList.add("active");
+    } else if (location.pathname === "/public/competences.html") {
+        comp_link.classList.add("active");
+    } else if (location.pathname === "/public/experiences.html") {
+        exp_link.classList.add("active");
     }
 })();
